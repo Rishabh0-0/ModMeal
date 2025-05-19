1dea9256d28973d80649a2f1abc80905c86a5a46
Rishabh Raj <rajrishabh2002711@gmail.com>
2025-05-19 19:44:32 +0530

    added separate ingredients schema and total time as virtuals

--- backend/models/RecipeModel.js
+++ backend/models/RecipeModel.js
@@ -1,40 +1,70 @@
 const mongoose = require("mongoose");
 
-const recipeSchema = new mongoose.schema({
+//////////////////////////////////////////////////////////////
+/////////////////////// Ingredient Schema
+const ingredientSchema = new mongoose.schema({
   name: {
     type: String,
     required: true,
   },
-  description: String,
-  prepTime: {
-    type: Number,
-    required: true,
+  quantity: Number,
+  unit: String,
+  prepration: String,
+  optional: {
+    type: Boolean,
+    default: false,
   },
-  cookTime: {
-    type: Number,
-    required: true,
-  },
-  totalTime: Number,
-  recipeYield: String,
-  recipeCategory: String,
-  recipeCuisine: String,
-  keywords: [String],
-  images: [String], // URL
-  nutrition: {
-    calories: String,
-    fatContent: String,
-    proteinContent: String,
-    carbohydrateContent: String,
-  },
-  recipeIngredients: [String], // Array of Ingredents list
-  recipeInstruction: [
-    {
-      step: Number,
-      text: String,
-    },
-  ],
-  tips: String,
 });
+
+//////////////////////////////////////////////////////////////
+/////////////////////// Recipe Schema
+const recipeSchema = new mongoose.schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    prepTime: {
+      type: Number,
+      required: true,
+    },
+    cookTime: {
+      type: Number,
+      required: true,
+    },
+    recipeYield: String,
+    recipeCategory: String,
+    recipeCuisine: String,
+    keywords: [String],
+    images: [String], // URL
+    nutrition: {
+      calories: String,
+      fatContent: String,
+      proteinContent: String,
+      carbohydrateContent: String,
+    },
+    recipeIngredients: [ingredientSchema], // Array of Ingredents
+    recipeInstruction: [
+      {
+        step: Number,
+        text: String,
+      },
+    ],
+    tips: String,
+  },
+  {
+    toJSON: { virutals: true },
+  }
+);
+
+//////////////////////////////////////////////////////////////
+/////////////////////// Virtuals
+recipeSchema.virtual("totalTime").get(function () {
+  return this.prepTime + this.cookTime;
+});
+
+//////////////////////////////////////////////////////////////
 
 const Recipe = mongoose.model("Recipe", recipeSchema);
 module.exports = Recipe;
